fa3857cc3072c27f5a71680d911e0dd1f552f4f2
NotWendy <47554589+NotWendy@users.noreply.github.com>
2021-10-12 17:19:28 +0300

    Finished module 5

--- SEF - Participants Workspace/SEF/src/sef/module5/sample/WorkerFactory.java
+++ SEF - Participants Workspace/SEF/src/sef/module5/sample/WorkerFactory.java
@@ -4,6 +4,7 @@ public class WorkerFactory {
 
 	public static final String TYPE_A="A";
 	public static final String TYPE_B="B";
+	public static final String TYPE_C="C";
 	
 	
 	public static Worker createWorker(String type){
@@ -12,6 +13,8 @@ public class WorkerFactory {
 			return new WorkerImplA();
 		}else if (type.equals("B")){
 			return new WorkerImplB();
+		}else if (type.equals("C")){
+			return new WorkerImplC();
 		}else{
 			return null;
 		}
